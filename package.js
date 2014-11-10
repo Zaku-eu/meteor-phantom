447c5d3d07fb0a0700519c360c080d92a20ebb58
Tamino Martinius <dev@zaku.eu>
2014-11-10 10:32:26 +0100

    updated to METEOR@0.9.0

--- package.js
+++ package.js
@@ -1,14 +1,10 @@
 Package.describe({
-    summary: "This is a tiny phantom wrapper"
+  summary: 'This is a tiny phantom wrapper',
+  version: '0.9.0',
+  git: 'https://github.com/zaku-eu/meteor-phantom.git'
 });
 
-Npm.depends({
-  "shoe": "https://github.com/bluekeyes/shoe/tarball/45429420fece517b6df9ffc856aa5ba661a10741",
-  "phantom": "0.6.1"
-});
-
-// even though we're serving minified, dynamic loading would be nice
-Package.on_use(function (api) {
-  api.export('phantom');
-  api.add_files('phantom.js', 'server');
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0');
+  api.addFiles('zaku:phantom.js');
 });
